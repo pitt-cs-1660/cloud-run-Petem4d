66e2c8c940fbce5d96b0d97049770c6ba623e752
Peter Ford <91855543+Petem4d@users.noreply.github.com>
2025-03-23 22:24:11 -0400

    initial changes

--- cc_cloud_run/main.py
+++ cc_cloud_run/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Form, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from google.cloud import firestore
+from fastapi.responses import RedirectResponse
+
 from typing import Annotated
 import datetime
 
@@ -23,15 +25,29 @@ async def read_root(request: Request):
     # ====================================
 
     # stream all votes; count tabs / spaces votes, and get recent votes
-
+    # get all votes from firestore collection
+    votes = votes_collection.stream()
+    # @note: we are storing the votes in `vote_data` list because the firestore stream closes after certain period of time
+    vote_data = []
+    space_count = 0
+    tab_count = 0
+    for v in votes:
+        temp = v.to_dict()
+        if(temp["team"] == "TABS"):
+            tab_count += 1
+        else:
+            space_count += 1
+        vote_data.append(temp)
+        
+    #print(vote_data)
     # ====================================
     # ++++ STOP CODE ++++
     # ====================================
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "tabs_count": 0,
-        "spaces_count": 0,
-        "recent_votes": []
+        "tabs_count": tab_count,
+        "spaces_count": space_count,
+        "recent_votes": vote_data
     })
 
 
@@ -43,9 +59,19 @@ async def create_vote(team: Annotated[str, Form()]):
     # ====================================
     # ++++ START CODE HERE ++++
     # ====================================
+    
+    votes_collection.add({
+        "team": team,
+        "time_cast": datetime.datetime.utcnow().isoformat()
+    })
+    #return RedirectResponse(url=f"/", status_code=303)
 
-    # create a new vote document in firestore
-    return {"detail": "Not implemented yet!"}
+
+        # create a new vote document in firestore
+    # return     votes_collection.add({
+    #     "team": team,
+    #     "time_cast": datetime.datetime.utcnow().isoformat()
+    # })
 
     # ====================================
     # ++++ STOP CODE ++++
--- static/config.js
+++ static/config.js
@@ -14,6 +14,6 @@
  * @type {{apiKey: string, authDomain: string}}
  */
 const config = {
-  apiKey: 'AIzaSyDhwsk8AklD8Ipj26fVzYTMgrWvE8x1rgc',
-  authDomain: 'cc-spring2025.firebaseapp.com',
+  apiKey: 'AIzaSyD7N-tmaWMDQzWPWI4WpqxUgRM5xZASDew',
+  authDomain: 'cloud-run-454701.firebaseapp.com',
 };
